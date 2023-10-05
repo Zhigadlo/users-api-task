61f057b1014b96009c3c896920094a7c87606039
Vladislav Zhogol <vzhogol176@gmail.com>
2023-10-05 23:40:52 +0300

    Create validator for models

---
+++ users-api/users-api.BLL/Validation/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using users_api.DAL.Models;
+
+namespace users_api.BLL.Validation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator() 
+        {
+            RuleFor(u => u.Name).NotEmpty();
+            RuleFor(u => u.Age).Must(a => a > 0);
+            RuleFor(u => u.Role).NotNull().IsInEnum();
+            RuleFor(u => u.Email).NotEmpty().EmailAddress();
+        }
+    }
+}
