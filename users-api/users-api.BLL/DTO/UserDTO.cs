598468363bb7c0633caf8a1914dc7063882dbc26
Vladislav Zhogol <vzhogol176@gmail.com>
2023-10-05 23:39:57 +0300

    DTO models creation

---
+++ users-api/users-api.BLL/DTO/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace users_api.BLL.DTO
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Role { get; set; }
+    }
+}
