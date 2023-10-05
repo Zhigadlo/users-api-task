3304cd339351514bc48d11c8d70c10c867d437d5
Vladislav Zhogol <vzhogol176@gmail.com>
2023-10-05 22:23:01 +0300

    Entity context and models configuration create

---
+++ users-api/users-api.DAL/EF/EntityTypeConfigurations/UserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using users_api.DAL.Models;
+
+namespace users_api.DAL.EF.EntityTypeConfigurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id);
+            builder.Property(x => x.Name);
+            builder.Property(x => x.Age);
+            builder.Property(x => x.Email);
+            builder.Property(x => x.Role);
+        }
+    }
+}
---
+++ users-api/users-api.DAL/EF/UsersContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using users_api.DAL.EF.EntityTypeConfigurations;
+using users_api.DAL.Models;
+
+namespace users_api.DAL.EF
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+}
