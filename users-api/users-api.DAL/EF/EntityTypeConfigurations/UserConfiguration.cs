8f4e9e69b8f6c800a709ee7f1e8e9fec63a90eea
Vladislav Zhogol <vzhogol176@gmail.com>
2023-10-06 22:58:16 +0300

    Add some initial data for configuration

--- users-api/users-api.DAL/EF/EntityTypeConfigurations/UserConfiguration.cs
+++ users-api/users-api.DAL/EF/EntityTypeConfigurations/UserConfiguration.cs
@@ -14,6 +14,40 @@ namespace users_api.DAL.EF.EntityTypeConfigurations
             builder.Property(x => x.Age);
             builder.Property(x => x.Email);
             builder.Property(x => x.Role);
+            builder.HasData(
+                new User()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Vladislav",
+                    Age = 20,
+                    Email = "vladislav@gmail.com",
+                    Role = Role.User
+                },
+                new User()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Ivan",
+                    Age = 21,
+                    Email = "ivan@gmail.com",
+                    Role = Role.Support
+                },
+                new User()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Aleksandr",
+                    Age = 22,
+                    Email = "aleksandr@gmail.com",
+                    Role = Role.SuperAdmin
+                },
+                new User()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Admin",
+                    Age = 35,
+                    Email = "admin@gmail.com",
+                    Role = Role.Admin
+                }
+            );
         }
     }
 }
