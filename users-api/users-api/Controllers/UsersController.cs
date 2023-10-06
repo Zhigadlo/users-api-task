6906d67768e25972ff2bef9d3705c68b608d74a0
Vladislav Zhogol <vzhogol176@gmail.com>
2023-10-06 22:59:53 +0300

    Add users controller and get all users method

---
+++ users-api/users-api/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using users_api.BLL.Interfaces;
+using users_api.BLL.Services;
+
+namespace users_api.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : Controller
+    {
+        private UserService _service;
+        private ILoggerManager _loggerManager;
+        public UsersController(UserService service, ILoggerManager loggerManager)
+        {
+            _service = service;
+            _loggerManager = loggerManager;
+        }
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            try
+            {
+                var companies = _service.GetAll(false);
+                return Ok(companies);
+            }
+            catch (Exception ex)
+            {
+                _loggerManager.LogError($"Something went wrong in the {nameof(GetUsers)} action {ex}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
