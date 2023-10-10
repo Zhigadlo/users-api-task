269a5055c4ce5294e6f99e5554db09400fd37435
Vladislav Zhogol <vzhogol176@gmail.com>
2023-10-10 16:07:16 +0300

    connecting all necessery services

--- users-api/users-api/Program.cs
+++ users-api/users-api/Program.cs
@@ -1,33 +1,41 @@
 using AutoMapper;
+using Contracts;
 using Microsoft.EntityFrameworkCore;
 using NLog;
 using NLog.Web;
-using users_api.BLL.Interfaces;
 using users_api.BLL.Managers;
 using users_api.BLL.Mapper;
 using users_api.BLL.Services;
 using users_api.DAL.EF;
+using users_api.DAL.Managers;
 
 var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 logger.Debug("init main");
+string path = string.Concat(Directory.GetCurrentDirectory(), "/nlog.config");
+LogManager.Setup().LoadConfigurationFromFile(path);
 
 try
 {
-
     var builder = WebApplication.CreateBuilder(args);
 
     builder.Logging.ClearProviders();
     builder.Host.UseNLog();
-    string path = string.Concat(Directory.GetCurrentDirectory(), "/nlog.config");
-    LogManager.LoadConfiguration(path);
+
     builder.Services.AddDbContext<UsersContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"),
                                                 b => b.MigrationsAssembly("users-api")));
     builder.Services.AddControllers();
 
-    builder.Services.AddScoped<UserService>();
-    var config = new MapperConfiguration(cfg => cfg.AddProfile(new MapperProfile()));
+    builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+    var config = new MapperConfiguration(cfg => cfg.AddProfiles(new List<Profile> 
+    { 
+        new RoleProfile(),
+        new UserProfile(),
+        new UserRoleProfile() 
+    }));
     builder.Services.AddScoped<IMapper>(x => new Mapper(config));
     builder.Services.AddScoped<ILoggerManager, LoggerManager>();
+    builder.Services.AddScoped<IServiceManager, ServiceManager>();
+    builder.Services.AddScoped<UserService>();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
