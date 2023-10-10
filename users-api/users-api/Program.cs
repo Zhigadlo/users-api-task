5e739a9063dc2fc7e36bc17a85c26278a2e0a679
Vladislav Zhogol <vzhogol176@gmail.com>
2023-10-10 20:57:37 +0300

    update nlog.config

--- users-api/users-api/Program.cs
+++ users-api/users-api/Program.cs
@@ -10,6 +10,7 @@ using users_api.BLL.Mapper;
 using users_api.BLL.Services;
 using users_api.DAL.EF;
 using users_api.DAL.Managers;
+using users_api.Extensions;
 
 var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 logger.Debug("init main");
@@ -35,13 +36,16 @@ try
         new UserRoleProfile()
     }));
     builder.Services.AddScoped<IMapper>(x => new Mapper(config));
-    builder.Services.AddScoped<ILoggerManager, LoggerManager>();
+    builder.Services.AddTransient<ILoggerManager, LoggerManager>();
     builder.Services.AddScoped<IServiceManager, ServiceManager>();
     builder.Services.AddScoped<UserService>();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
 
     var app = builder.Build();
+    var loggerManager = app.Services.GetRequiredService<ILoggerManager>();
+    app.ConfigureExceptionHandler(loggerManager);
+
 
     if (app.Environment.IsDevelopment())
     {
@@ -59,12 +63,10 @@ try
 }
 catch (Exception exception)
 {
-    // NLog: catch setup errors
     logger.Error(exception, "Stopped program because of exception");
     throw;
 }
 finally
 {
-    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-    NLog.LogManager.Shutdown();
+    LogManager.Shutdown();
 }
