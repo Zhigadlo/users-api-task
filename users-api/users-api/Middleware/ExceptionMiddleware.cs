bafe02e9b2a8c7a2ce7a9bb44d5475f0fd4de58d
Vladislav Zhogol <vzhogol176@gmail.com>
2023-10-11 00:35:37 +0300

    Create middleware  for exception handling

---
+++ users-api/users-api/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Entities.ErrorModels;
+using Entities.Exceptions;
+using System.Net;
+
+namespace users_api.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, ILogger<ExceptionMiddleware> logger)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+                logger.LogError(ex, ex.Message);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            var statusCode = exception switch
+            {
+                BadRequestException => HttpStatusCode.BadRequest,
+                NotFoundException => HttpStatusCode.NotFound,
+                _ => HttpStatusCode.InternalServerError
+            };
+
+            context.Response.StatusCode = (int)statusCode;
+            var errorDetails = new ErrorDetails() { StatusCode = (int)statusCode, Message = exception.Message };
+            await context.Response.WriteAsync(errorDetails.ToString());
+        }
+    }
+}
